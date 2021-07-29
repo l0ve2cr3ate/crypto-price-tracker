f8b4e9b46f6bd761de2290934c294f075779f2b8
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-07-29 10:13:30 +0200

    feat: Create Homepage with BottomSheet to display crypto currency data using FlatList.

--- App.js
+++ App.js
@@ -1,21 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useMemo } from "react";
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
+
+import ListItem from "./components/ListItem";
+import { SAMPLE_DATA } from "./assets/data/sampleData";
+
+const ListHeader = () => (
+  <>
+    <View style={styles.titleWrapper}>
+      <Text style={styles.largeTitle}>Markets</Text>
+    </View>
+    <View style={styles.divider} />
+  </>
+);
 
 export default function App() {
+  const bottomSheetModalRef = useRef(null);
+  const snapPoints = useMemo(() => ["50%"], []);
+
+  const openModal = () => {
+    bottomSheetModalRef.current?.present();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomSheetModalProvider>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={SAMPLE_DATA}
+          renderItem={({ item }) => (
+            <ListItem
+              name={item.name}
+              symbol={item.symbol}
+              currentPrice={item.current_price}
+              priceChangePercentage7d={
+                item.price_change_percentage_7d_in_currency
+              }
+              logoUrl={item.image}
+              onPress={() => openModal()}
+            />
+          )}
+          ListHeaderComponent={ListHeader}
+        />
+      </SafeAreaView>
+
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0}
+        snapPoints={snapPoints}
+        style={styles.bottomSheet}
+      >
+        <View style={styles.contentContainer}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  titleWrapper: {
+    marginTop: 20,
+    paddingHorizontal: 16,
+  },
+  largeTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: "#A9ABB1",
+    marginHorizontal: 16,
+    marginTop: 16,
+  },
+  bottomSheet: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: -4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
